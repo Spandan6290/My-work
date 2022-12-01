812cf7489aecd16bf8e102cc947c549c00b3236d
Spandan6290 <112849466+Spandan6290@users.noreply.github.com>
2022-11-30 21:30:44 -0800

    Add files via upload

---
+++ rana.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a[]={12,23,1000,25};
+int length = sizeof(a)/sizeof(a[0]);
+printf("no. of element in array  =  %d",length);
+return 0;
+
+
+
+
+  
+} 
+
+
+
+
+
